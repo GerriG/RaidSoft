8083261dba1f05f398892af8c94f6016f8737e6b
GerriG <gerrigerard1000@gmail.com>
2025-11-19 15:14:06 -0600

    RaidSoft - SpringBoot Version
    New Branch of the Development the RaidSoft Stock Manager on SpringBoot

---
+++ RaidSoft/src/main/java/com/raidsoft/RaidSoftApplication.java
@@ -0,0 +1,13 @@
+package com.raidsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidSoftApplication.class, args);
+	}
+
+}
---
+++ RaidSoft/src/test/java/com/raidsoft/RaidSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
